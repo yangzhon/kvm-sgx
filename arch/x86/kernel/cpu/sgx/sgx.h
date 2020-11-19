sean-jc/linux

x86/sgx: Split out adding EPC page to free list to separate helper
Split sgx_free_page() into two parts so that the "add to free list"
part can be used by virtual EPC without having to modify the EREMOVE
logic in sgx_free_page().  Virtual EPC does not track (de)construction
of enclaves and so cannot guarantee EREMOVE success, e.g. it doesn't
have a priori knowledge of which pages are SECS pages with non-zero
child counts.

Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>

arch/x86/kernel/cpu/sgx/sgx.h
@@ -103,6 +103,7 @@ void sgx_reclaim_pages(void);
 
 struct sgx_epc_page *sgx_try_alloc_page(void);
 struct sgx_epc_page *sgx_alloc_page(void *owner, bool reclaim);
+void __sgx_free_page(struct sgx_epc_page *page);
 void sgx_free_page(struct sgx_epc_page *page);
 
 int sgx_set_attribute(u64 *allowed_attributes, unsigned int attribute_fd);

yangzhon/kvm-sgx

arch/x86/kernel/cpu/sgx/sgx.h
@@ -68,6 +68,7 @@ static inline void *sgx_get_epc_virt_addr(struct sgx_epc_page *page)
 }
 
 struct sgx_epc_page *__sgx_alloc_epc_page(void);
+void __sgx_free_epc_page(struct sgx_epc_page *page);
 void sgx_free_epc_page(struct sgx_epc_page *page);
 
 void sgx_mark_page_reclaimable(struct sgx_epc_page *page);